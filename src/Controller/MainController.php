cdc640338c77caa491a5d082a86599c4afe54e66
Gael Herambourg <g.herambourg.gh@gmail.com>
2021-01-27 15:28:24 +0100

    Affichage des données de BDD

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -36,20 +36,44 @@ class MainController extends AbstractController
     public function testData()
     {
         //Créer une instance de la classe
-        $wish = new Wish();
+        $wish1 = new Wish();
+        $wish2 = new Wish();
+        $wish3 = new Wish();
+        $wish4 = new Wish();
 
         //hydrate chacune de ses propriété requises
-        $wish->setTitle('South America');
-        $wish->setDescription('Partir en famille 1 an minimum en Amérique du sud en itinérant');
-        $wish->setAuthor('Gael H.');
-        $wish->setIsPublished(false);
-        $wish->setDateCreated(new \DateTime());
+        $wish1->setTitle('Nager avec les dauphins');
+        $wish1->setDescription('blaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa a a a a aaaaa a aaaaa');
+        $wish1->setAuthor('Gael H.');
+        $wish1->setIsPublished(true);
+        $wish1->setDateCreated(new \DateTime());
+
+        $wish2->setTitle('Surfer au Mexique');
+        $wish2->setDescription('gfzjfezkjnflkrezjfkrezj flkehjzfozehfez');
+        $wish2->setAuthor('Gael H.');
+        $wish2->setIsPublished(true);
+        $wish2->setDateCreated(new \DateTime());
+
+        $wish3->setTitle('Faire du canyoning');
+        $wish3->setDescription('fezkhzfiuyoiezfoirezf');
+        $wish3->setAuthor('Gael H.');
+        $wish3->setIsPublished(true);
+        $wish3->setDateCreated(new \DateTime());
+
+        $wish4->setTitle('Parachute');
+        $wish4->setDescription('Sauter en parachute');
+        $wish4->setAuthor('Gael H.');
+        $wish4->setIsPublished(true);
+        $wish4->setDateCreated(new \DateTime());
 
         //on appelle l'entity manager de doctrine (on peut également le faire avec le passage en parametre de la fonction)
         $entityManager = $this->getDoctrine()->getManager();
 
         //On demande à doctrine de sauvegarder notre instance
-        $entityManager->persist($wish);
+        $entityManager->persist($wish1);
+        $entityManager->persist($wish2);
+        $entityManager->persist($wish3);
+        $entityManager->persist($wish4);
         //On execute la requete sql maintenant
         $entityManager->flush();
 
--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\WishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,19 +12,29 @@ class WishController extends AbstractController
     /**
      * @Route("/list", name="wish_list")
      */
-    public function list()
+    public function list(WishRepository $wishRepository)
     {
         //todo: aller chercher tous les wishes dans la BDD
-        return $this->render('wish/list.html.twig');
+        //$wishes = $wishRepository->findAll();
+        $wishes = $wishRepository->findBy(["isPublished"=>true], ["dateCreated"=>"DESC"], 200,0);
+        //->findOneBy();
+        //dd($wishes);
+        //$a = $wishRepository->findByIsPublished(1);
+        //dd($a);
+        //$wish1 = $wishRepository->find(1);
+
+
+        return $this->render('wish/list.html.twig', ['wishes'=>$wishes]);
     }
 
     /**
      * @Route("/list/detail/{id}", name="wish_detail", requirements={"id": "\d+"})
      */
-    public function detail($id)
+    public function detail($id, WishRepository $wishRepository)
     {
         //todo: aller chercher dans la BDD le souhait dont l'id est dans l'url
-        return $this->render('wish/detail.html.twig',['id' => $id]);
+        $w = $wishRepository->find($id);
+        return $this->render('wish/detail.html.twig',['id' => $id, 'wish1'=>$w]);
     }
 
 
